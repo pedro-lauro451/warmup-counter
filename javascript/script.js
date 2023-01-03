7e2d2a536b26b84add0cafa628cafbfd09547492
Pedro <jinnpheonix@protonmail.com>
2023-01-03 17:20:41 -0300

    leave trash icon

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
 var input = document.getElementById('input');
+var inputDiv = document.getElementById('input-label');
 var gmail_wp = document.getElementById('gmail_wp');
 var hotmail_wp = document.getElementById('hotmail_wp');
 var yahoo_wp = document.getElementById('yahoo_wp');
@@ -21,26 +22,52 @@ var total_others = [];
 var length = 0;
 var total = 0;
 
+var i = 0;
 var y = 0;
 
 var isActive = false;
 
-var colors = ["#B6FCE8", "#7CE964", "#EAE575", "#EAB375", "#7590EA", "#D775EA", "#75EACC", "#E18C7D", "#52A9D2", "#8e6dcf", "#d989c9", "#099FFF", "#67c79c", "#d64f4f"];
+var colors = [
+"#B6FCE8", 
+"#7CE964", 
+"#EAE575", 
+"#EAB375", 
+"#7590EA", 
+"#D775EA", 
+"#75EACC", 
+"#E18C7D", 
+"#52A9D2", 
+"#8e6dcf", 
+"#d989c9", 
+"#099FFF", 
+"#67c79c", 
+"#d64f4f"];
+
+function hover(img)
+{
+    img.src = "images/trash-empty-svgrepo-com.svg";
+}
+
+function leave(img)
+{
+    img.src = "images/trash-svgrepo-com.svg";
+}
 
 function deleteFilters(id) {
     while (id.firstChild) {
         id.removeChild(id.firstChild);
     }
-    input.value = null;
-    isActive = false;
 }
 
 function useDeleteFilters()
 {
+    inputDiv.style.display = "initial";
     deleteFilters(gmail_wp);
     deleteFilters(hotmail_wp);
     deleteFilters(yahoo_wp);
     deleteFilters(others_wp);
+    input.value = null;
+    isActive = false;
 }
 
 function calculateFilters(array, warmupLength, id, total) {
@@ -51,7 +78,7 @@ function calculateFilters(array, warmupLength, id, total) {
     var d = 1;
     var campaign = 0;
 
-    for (var i = 0; i < warmupLength; i++) {
+    for (i = 0; i < warmupLength; i++) {
         if (array[i] + initialCount > total) {
             initialCount = 1;
             campaign = campaign + 1;
@@ -71,6 +98,7 @@ function calculateFilters(array, warmupLength, id, total) {
 
 function useCalculateFilters()
 {
+    inputDiv.style.display = "none";
     calculateFilters(gmail, length, gmail_wp, total_gmail);
     calculateFilters(hotmail, length, hotmail_wp, total_hotmail);
     calculateFilters(yahoo, length, yahoo_wp, total_yahoo);
